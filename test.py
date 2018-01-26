b5b80d9e82b31e65667e475a60aa36a2f311047b
AndreBest <35811429+AndreBest@users.noreply.github.com>
2018-01-26 19:43:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import sys
 import time
 import random
 
+weapon = False
 bar = 75
 
 print '{................................Loading Game...............................}\n'
@@ -18,20 +19,14 @@ for i in xrange(bar):
     sys.stdout.flush()
 sys.stdout.write("\n")
 
-def try_again_1():
-    prompt = '> '
-    print 'Mai mere un fms ?'
-    print '1. Mere'
-    print '2. Nu mere'
-    mere = int(input(prompt))
-    if mere == 1:
-        search_wp()
-    if mere == 2:
-        exit(0)
-    else:
-        print 'Nu am vz in viata mea o optiune ca asta coaie ce pula mea ?'
 def dick():
     print 'Satana mere nu ioo'
+
+def dye():
+    print 'Imi pare bine ca ai jucat'
+    print 'Dar esti praf'
+    time.sleep(5)
+    exit(0)
 
 def school():
     print 'Scoala'
@@ -39,38 +34,132 @@ def school():
 def main2():
     prompt = '> '
     print 'Unde vrei sa meri?'
-    print '1. Pula'
-    print '2. Scoala'
-    print '3. Exit'
+    print '1. Temnita'
+    print '2. Pula'
+    print '3. Scoala'
+    print '4. Exit'
     
     index = int(input(prompt))
-    
+
     if index == 1:
-        dick()
+        time.sleep(2)
+        dungeon()
     elif index == 2:
-        school()
+        time.sleep(2)
+        dick()
     elif index == 3:
+        time.sleep(2)
+        school()
+    elif index == 4:
+        time.sleep(2)
         exit(0)
     else:
         print ('ERROR 404 : Invalid option')
 
 def dungeon():
     prompt = '> '
-    boss = 2631
+    player = random.randint(1000,3000)
+    boss = random.randint(1000,3000)
+    print 'Your Health %d' % player
     print 'Boss Health %d' % boss
-    print 'COAIE E UN BOSS AICI CAT PULA LUI DUMNEZEU CE O SA FACI ?'
+    print 'Aici e un boss ce o sa faci ?'
+    print '1. Pumn in coaie cu fms'
+    print '2. RUN BITCH RUUUUUUN'
+    decision = int(input(prompt))
+    if decision == 1:
+        while True:
+            damage = random.randint(623, 1600)
+            damage_boss = random.randint(623, 1600)
+            time.sleep(0.5)
+            print 'Vrei sa il ataci sau sa te retragi ?'
+            print '1. Ataca'
+            print '2. Retragete'
+            ppwp = int(input(prompt))
+            if ppwp == 1:
+                print 'i-ai dat %d damage' % damage
+                time.sleep(0.5)
+                print 'boss-ul mai are %d viata' % boss
+                boss -= damage
+                print 'ti-a dat %d damage' % damage_boss
+                time.sleep(0.5)
+                print 'mai ai %d viata' % player
+                player -= damage_boss
+                time.sleep(0.5)
+                if boss < 10:
+                    print 'ai infrant boss-ul'
+                    time.sleep(5)
+                    main2()
+                if player < 10:
+                    print 'ai murit....'
+                    time.sleep(5)
+                    dye()
+            elif ppwp ==2:
+                time.sleep(5)
+                main()
+            else:
+                print ('ERROR 404 : Invalid option')
+
+    elif decision == 2:
+        time.sleep(5)
+        exit(0)
+    else:
+        print ('ERROR 404 : Invalid option')
+
+def dungeon1_go():
+    print 'esti sigur ca vrei sa merem la boss?'
+    print '1. Da'
+    print '2. Ne intoarcem la meniul principal'
+    go = int(input(prompt))
+    if go == 1:
+        time.sleep(5)
+        dungeon1()
+    elif go == 2:
+        time.sleep(5)
+        main()
+    else:
+        print ('ERROR 404 : Invalid option')
+
+def dungeon1():
+    prompt = '> '
+    player = random.randint(500,800)
+    boss = random.randint(1000,3000)
+    print 'Your Health %d' % player
+    print 'Boss Health %d' % boss
+    print 'Aici e un boss ce o sa faci ?'
     print '1. Pumn in coaie'
     print '2. RUN BITCH RUUUUUUN'
     decision = int(input(prompt))
-    if decision == 1 and weapon or False:
-        damage in range(623, 1600)
-        print 'i-ai dat %d damage' % damage
-        boss -= damage
-        time.sleep(0.5)
-        if boss < 1:
-            print 'you beat the boss'
-            time.sleep(5)
-            main2()
+    if decision == 1:
+        while True:
+            damage = random.randint(11,23)
+            damage_boss = random.randint(500,800)
+            time.sleep(0.5)
+            print 'Vrei sa il ataci sau sa te retragi ?'
+            print '1. Ataca'
+            print '2. Retragete'
+            ppwp = int(input(prompt))
+            if ppwp == 1:
+                print 'i-ai dat %d damage' % damage
+                boss -= damage
+                print 'ti-a dat %d damage' % damage_boss
+                player -= damage_boss
+                time.sleep(0.5)
+                if boss < 10:
+                    print 'ai batut boss-ul'
+                    time.sleep(5)
+                    main2()
+                elif player <10:
+                    print 'te-o facut ma'
+                    time.sleep(5)
+                    exit(0)
+            elif ppwp == 2:
+                time.sleep(5)
+                exit(0)
+    elif decision == 2:
+        time.sleep(5)
+        exit(0)
+    else:
+        print ('ERROR 404 : Invalid option')
 
 def search_wp():
     prompt = '> '
@@ -112,13 +201,23 @@ def search_wp():
                 print '\namin ca nu e +0\n'
             if fms < 5:
                 print 'fms-ul so spart trb sa gasesti minim unu +5 ca sa reziste la bosi'
-                print 'soo ne intoarcem'
-                search_wp()
+                print 'Ce vrei sa facem?'
+                print '1. Mergem La Boss'
+                print '2. Mai incercam o data'
+                wo = int(input(prompt))
+                if wo == 1:
+                    time.sleep(5)
+                    dungeon1_go()
+                elif wo == 2:
+                    time.sleep(5)
+                    search_wp()
+                else:
+                    print ('ERROR 404 : Invalid option')
             if fms >= 5:
                 print 'fms-ul e +%d destul si rezistent cat sa bata un boss' % fms
                 weapon = True
+                time.sleep(5)
                 main2()
-            time.sleep(5)
         elif l == 2:
             main()
         else:
@@ -144,7 +243,7 @@ def main():
         search_wp()
     elif index == 2:
         time.sleep(2)
-        dungeon()
+        dungeon1()
     elif index == 3:
         time.sleep(2)
         dick()
